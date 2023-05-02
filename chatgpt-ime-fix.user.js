e92947f9fe99f8bfd90cc5097d4de22739f5cea1
rotsuya <r.otsuya@gmail.com>
2023-05-02 14:52:27 +0900

    wrote script

---
+++ chatgpt-ime-fix.user.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name         chagpt-ime-fix
+// @namespace    otsuya.co
+// @version      0.0.1
+// @description  Prevent message sending while using IME. This aims to improve the user experience when using ChatGPT with Mac Safari and Japanese IME.
+// @author       rotsuya
+// @match        https://chat.openai.com/*
+// @grant        none
+// @updateURL    https://raw.githubusercontent.com/rotsuya/userscripts/chatgpt-ime-fix.user.js
+// @downloadURL  https://raw.githubusercontent.com/rotsuya/userscripts/chatgpt-ime-fix.user.js
+// @supportURL   https://github.com/rotsuya/userscripts/
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function handleEnterWithIME(event) {
+        if (
+            event.target.tagName === 'TEXTAREA' &&
+            event.code === 'Enter' &&
+            (event.isComposing || event.key === 'Process' || event.keyCode === 229)
+        ) {
+            event.stopPropagation();
+        }
+    }
+
+    function enableIMEAwareSending() {
+        document.addEventListener('keydown', handleEnterWithIME, {capture: true});
+        console.log('addEventListener');
+    }
+
+    enableIMEAwareSending();
+})();
